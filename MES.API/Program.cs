d49f0c0a792f0729f5ad6ae9f03e3f224ad62f87
athallaarl66 <athallaarli@gmail.com>
2026-03-23 16:51:58 +0700

    programcs fix untuk validators

--- MES.API/Program.cs
+++ MES.API/Program.cs
@@ -5,22 +5,39 @@ using Microsoft.EntityFrameworkCore;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using MES.API.Validators;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// validators workorders gasingkron
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var errors = context.ModelState
+                .Where(e => e.Value?.Errors.Count > 0)
+                .SelectMany(e => e.Value!.Errors)
+                .Select(e => e.ErrorMessage)
+                .ToList();
+
+            return new BadRequestObjectResult(new
+            {
+                success = false,
+                message = "Validasi gagal",
+                errors
+            });
+        };
+    });
+
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateWorkOrderValidator>();
 
 builder.Services.AddDbContext<MesDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-// workorder
+
 builder.Services.AddScoped<IWorkOrderService, WorkOrderService>();
-// stepservice
 builder.Services.AddScoped<IStepService, StepService>();
 
-// builder
 var app = builder.Build();
 
 app.UseHttpsRedirection();
